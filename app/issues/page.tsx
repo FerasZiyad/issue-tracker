031ed208998996208fb501e2940e2b925203b078
FerasZiyad <feraswarak@hotmail.com>
2024-01-15 09:38:10 +0400

    Installed RadixUI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const issuesPage = () => {
-  return <div>Issues</div>;
+  return <Button>New Issue</Button>;
 };
 
 export default issuesPage;
