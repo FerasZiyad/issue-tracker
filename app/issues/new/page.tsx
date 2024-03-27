7a5d69f03f2ffe92657522b205ec4c94b34484cd
FerasZiyad <feraswarak@hotmail.com>
2024-03-28 00:40:34 +0400

    Handle form submission and edit enum on prisma schema

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import { Button, TextFieldInput } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextFieldInput placeholder="Title" />
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextFieldInput placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
